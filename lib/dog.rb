0d00d430a7dc246443bc97ec892e001428cf374c
GeneHFang <ghl234@nyu.edu>
2019-10-08 11:04:54 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+    def name= (pname)
+        @name = pname
+    end
+
+    def name
+        @name
+    end
+
+    def breed= (pbreed)
+        @breed = pbreed 
+    end
+
+    def breed
+        @breed
+    end
+    
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person 
+    def name= (pname)
+        @name = pname
+    end
+
+    def name
+        @name
+    end
+
+    def job= (pjob)
+        @job = pjob 
+    end
+
+    def job
+        @job
+    end
+    
+
+end
+
