7cee26b091f76e14901c3ad32d5febce709fa36b
brindab50 <brindab50@gmail.com>
2019-10-08 21:53:30 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,17 +12,4 @@ class Dog
         @breed
     end
 end 
- class Person
-    def name=(persons_name)
-        @name = persons_name
-    end 
-    def name 
-        @name
-    end
-    def job=(persons_job)
-    @job = persons_job
-    end 
-    def job
-        @job
-    end
-end
+ 
